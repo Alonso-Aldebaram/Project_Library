76e8ad278fa88532d15088504358ba5d963c90ff
Alonso-Aldebaram <abraabramixx@gmail.com>
2020-08-05 14:42:27 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function Book(titulo, autor, paginas, leido) {
+    this.titulo = titulo
+    this.autor = autor
+    this.paginas = paginas
+    this.leido = leido
+    this.info = function() {
+        console.log(titulo, autor, paginas, leido)
+    }
+}
+
+const book1 = new Book('CLEAN CODE', 'Robert C. Martin', '250', 'aún no leído')
+const book2 = new Book('THE CLEAN CODER', 'Robert C. Martin', '298', 'aún no leído')
+
+book1.info()
+book2.info()
