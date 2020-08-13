08120e062c92c36cf14e51092a5d92b0973c1d34
Alonso-Aldebaram <abraabramixx@gmail.com>
2020-08-13 13:22:07 -0500

    Implemente logica de los botones editar y eliminar

--- src/comp/Biblioteca.js
+++ src/comp/Biblioteca.js
@@ -14,14 +14,84 @@ const data = [
 
 class Biblioteca extends React.Component{
 state={
-    data: data
+    data: data,
+    form: {
+        id:'',
+        libro:'',
+        autor:'',
+        paginas:'',
+        estado:''
+    },
+    modalinsertar: false,
+    modalEditar: false,
+};
+
+handleChange=e=>{
+    this.setState({
+        form:{
+            ...this.state.form,
+            [e.target.name]: e.target.value,
+        }
+    });
+}
+
+mostrarModalInsertar=()=>{
+    this.setState({modalinsertar: true});
+}
+ocultarModalInsertar=()=>{
+    this.setState({modalinsertar: false});
+}
+
+mostrarModalEditar=(registro)=>{
+    this.setState({modalEditar: true, form: registro});
+}
+ocultarModalEditar=()=>{
+    this.setState({modalEditar: false});
+}
+
+insertar=()=>{
+    var valorNuevo={...this.state.form};
+    valorNuevo.id=this.state.data.length+1;
+    var lista=this.state.data;
+    lista.push(valorNuevo);
+    this.setState({data: lista, modalinsertar:false});
+}
+
+editar=(dato)=>{
+    var contador=0;
+    var lista=this.state.data;
+    lista.map((registro)=>{
+        if(dato.id==registro.id){
+            lista[contador].libro=dato.libro;
+            lista[contador].autor=dato.autor;
+            lista[contador].paginas=dato.paginas;
+            lista[contador].estado=dato.estado;
+        }
+        contador++;
+    });
+    this.setState({data:lista, modalEditar: false});
+}
+
+eliminar=(dato)=>{
+    var opcion=window.confirm("¿Deseas eliminar el libro "+dato.id+"?");
+    if(opcion){
+        var contador=0;
+        var lista = this.state.data;
+        lista.map((registro)=>{
+            if(registro.id==dato.id){
+                lista.splice(contador, 1);
+            }
+            contador++;
+        });
+        this.setState({data: lista});
+    }
 }
     render(){
         return(
             <>
             <Container>
             <br/>
-            <Button color="success">Nuevo Libro</Button>
+            <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Nuevo Libro</Button>
             <br/><br/>
             <Table>
                 <thead>
@@ -42,14 +112,14 @@ state={
                         <td>{elemento.autor}</td>
                         <td>{elemento.paginas}</td>
                         <td>{elemento.estado}</td>
-                        <td><Button color="primary">Editar</Button>{"  "}
-                            <Button color="danger">Eliminar</Button></td>
+                        <td><Button color="primary" onClick={()=>this.mostrarModalEditar(elemento)}>Editar</Button>{"  "}
+                            <Button color="danger" onClick={()=>this.eliminar(elemento)}>Eliminar</Button></td>
                         </tr>
                     ))}
                 </tbody>
             </Table>
             </Container>
-            <Modal>
+            <Modal isOpen={this.state.modalinsertar}>
                 <ModalHeader>
                     <div>
                         <h3>Insertar Libro</h3>
@@ -59,24 +129,90 @@ state={
                 <ModalBody>
                     <FormGroup>
                         <label>Id:</label>
-                        <input classname="formu-input" name="id" type="text"/>
+                        <input classname="formu-input" readOnly type="text" value={this.state.data.length+1}/>
                     </FormGroup>
 
                     <FormGroup>
                         <label>Libro:</label>
-                        <input className="formu-input" name="libro" type="text"/>
+                        <input className="formu-input" name="libro" type="text" onChange={this.handleChange}/>
                     </FormGroup>
 
                     <FormGroup>
                         <label>Autor:</label>
-                        <input className="formu-input" name="autor" type="text"/>
+                        <input className="formu-input" name="autor" type="text" onChange={this.handleChange}/>
                     </FormGroup>
 
                     <FormGroup>
                         <label>Paginas:</label>
-                        <input className="formu-input" name="autor" type="text"/>
+                        <input className="formu-input" name="paginas" type="text" onChange={this.handleChange}/>
+                    </FormGroup>
+                    
+                    <h5>Estado de lectura:</h5>
+                    <FormGroup>
+                        <label>
+                            <input className="formu-radio" name="estado" type="radio" value="Si leido" onChange={this.handleChange}/>{' '}
+                            Si leido
+                        </label>
+                        <br/>
+                        <label>
+                            <input className="Formu-radio" name="estado" type="radio" value="No leido" onChange={this.handleChange}/>{' '}
+                            No Leido
+                        </label>
                     </FormGroup>
                 </ModalBody>
+
+                <ModalFooter>
+                    <Button color="primary" onClick={()=>this.insertar()}>Agregar</Button>
+                    <Button color="danger" onClick={()=>this.ocultarModalInsertar()}>Cancelar</Button>
+                </ModalFooter>
+            </Modal>
+
+            <Modal isOpen={this.state.modalEditar}>
+                <ModalHeader>
+                    <div>
+                        <h3>Editar Libro</h3>
+                    </div>
+                </ModalHeader>
+
+                <ModalBody>
+                    <FormGroup>
+                        <label>Id:</label>
+                        <input classname="formu-input" readOnly type="text" value={this.state.form.id}/>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <label>Libro:</label>
+                        <input className="formu-input" name="libro" type="text" onChange={this.handleChange} value={this.state.form.libro}/>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <label>Autor:</label>
+                        <input className="formu-input" name="autor" type="text" onChange={this.handleChange} value={this.state.form.autor}/>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <label>Paginas:</label>
+                        <input className="formu-input" name="autor" type="text" onChange={this.handleChange} value={this.state.form.paginas}/>
+                    </FormGroup>
+
+                    <h5>Estado de lectura:</h5>
+                    <FormGroup>
+                        <label>
+                            <input className="formu-radio" name="estado" type="radio" value="Si leido" onChange={this.handleChange}/>{' '}
+                            Si leido
+                        </label>
+                        <br/>
+                        <label>
+                            <input className="Formu-radio" name="estado" type="radio" value="No leido" onChange={this.handleChange}/>{' '}
+                            No Leido
+                        </label>
+                    </FormGroup>
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button color="primary" onClick={()=>this.editar(this.state.form)}>Editar</Button>
+                    <Button color="danger" onClick={()=>this.ocultarModalEditar()}>Cancelar</Button>
+                </ModalFooter>
             </Modal>
             </>
         )
