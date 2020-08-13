b4592fd9190bfec4d1395b21ba55e3f00ad4d019
Alonso-Aldebaram <abraabramixx@gmail.com>
2020-08-13 10:04:51 -0500

    Implementando Logica

--- src/comp/Biblioteca.js
+++ src/comp/Biblioteca.js
@@ -4,18 +4,53 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter} from 'reactstrap';
 
 const data = [
-    { id: 1, libro: "CLEAN CODER", autor: "Robert C. Martin", paginas: "234", estado: "No leido" },
-    { id: 2, libro: "YOU DON’T KNOW JAVASCRIPT", autor: "Kyle Simpson", paginas: "655", estado: "No leido" },
-    { id: 3, libro: "ELOQUENT JAVASCRIPT", autor: "Marijn Haverbeke", paginas: "529", estado: "No leido" },
-    { id: 4, libro: "THE PRAGMATIC PROGRAMMER", autor: "Andrew Hunt y David Thomas", paginas: "789", estado: "No leido" },
-    { id: 5, libro: "DESIGN PATTERNS", autor: "Erich Gamma", paginas: "425", estado: "No leido"},
-    { id: 6, libro: "USER STORY MAPPING", autor: "Jeff Patton", paginas: "555", estado: "No leido" },
+    { id: 1,
+      libro: "CLEAN CODER",
+      autor: "Robert C. Martin",
+      paginas: "234",
+      estado: "No leido" },
+    { id: 2,
+      libro: "YOU DON’T KNOW JAVASCRIPT",
+      autor: "Kyle Simpson",
+      paginas: "655",
+      estado: "No leido" },
+    { id: 3, 
+      libro: "ELOQUENT JAVASCRIPT",
+      autor: "Marijn Haverbeke",
+      paginas: "529",
+      estado: "No leido" },
+    { id: 4,
+      libro: "THE PRAGMATIC PROGRAMMER",
+      autor: "Andrew Hunt y David Thomas",
+      paginas: "789",
+      estado: "No leido" },
+    { id: 5,
+      libro: "DESIGN PATTERNS",
+      autor: "Erich Gamma",
+      paginas: "425",
+      estado: "No leido"},
+    { id: 6,
+      libro: "USER STORY MAPPING",
+      autor: "Jeff Patton",
+      paginas: "555",
+      estado: "No leido" },
   ];
 
 class Biblioteca extends React.Component{
-state={
-    data: data
-}
+    constructor(props){
+        super(props)
+        this.state={}
+    }
+    // setData=(id, libro, autor, paginas, estado)=>{
+    //     this.setState({
+    //         selData: 
+    //         id,
+    //         libro,
+    //         autor,
+    //         paginas,
+    //         estado
+    //     })
+    // }
     render(){
         return(
             <>
@@ -35,13 +70,13 @@ state={
                     </tr>
                 </thead>
                 <tbody>
-                    {this.state.data.map((elemento)=>(
+                    {data.map(({id, libro, autor, paginas, estado})=>(
                         <tr>
-                        <td>{elemento.id}</td>
-                        <td>{elemento.libro}</td>
-                        <td>{elemento.autor}</td>
-                        <td>{elemento.paginas}</td>
-                        <td>{elemento.estado}</td>
+                        <td>{id}</td>
+                        <td>{libro}</td>
+                        <td>{autor}</td>
+                        <td>{paginas}</td>
+                        <td>{estado}</td>
                         <td><Button color="primary">Editar</Button>{"  "}
                             <Button color="danger">Eliminar</Button></td>
                         </tr>
@@ -59,7 +94,7 @@ state={
                 <ModalBody>
                     <FormGroup>
                         <label>Id:</label>
-                        <input classname="formu-input" name="id" type="text"/>
+                        <input classname="formu-input" readOnly type="text" value={this.state.data.length+1}/>
                     </FormGroup>
 
                     <FormGroup>
@@ -77,6 +112,11 @@ state={
                         <input className="formu-input" name="autor" type="text"/>
                     </FormGroup>
                 </ModalBody>
+
+                <ModalFooter>
+                    <Button color="primary">Agregar</Button>
+                    <Button color="danger">Cancelar</Button>
+                </ModalFooter>
             </Modal>
             </>
         )
